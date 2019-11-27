eb877524abc86476953f01b5346b43413cb93e15
hasezoey <hasezoey@gmail.com>
2019-11-27 11:56:18 +0100

    Upgrade Deps

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 // NodeJS: 12.13.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@6.1.1
-import * as mongoose from "mongoose"; // mongoose@5.7.9
+import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@6.1.5
+import * as mongoose from "mongoose"; // mongoose@5.7.12
 
 class User {
     @prop()
@@ -13,6 +13,7 @@ const UserModel = getModelForClass(User);
     await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
 
     const user = new UserModel({ username: "user1" });
+
     console.log(user);
 
     await mongoose.disconnect();
