724c5a60bd7026ad44576c317e6952593a891a38
hasezoey <hasezoey@gmail.com>
2020-05-12 13:38:23 +0200

    Update Dependencies

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
-// NodeJS: 12.16.2
+// NodeJS: 12.16.3
 // MongoDB: 4.2-bionic (Docker)
 import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.0.0
-import * as mongoose from "mongoose"; // mongoose@5.9.10
+import * as mongoose from "mongoose"; // mongoose@5.9.13 @types/mongoose@5.7.16
 
 class User {
   @prop()
@@ -12,9 +12,9 @@ const UserModel = getModelForClass(User);
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
 
-  const user = new UserModel({ username: "user1" });
+  const doc = new UserModel({ username: "user1" });
 
-  console.log(user);
+  console.log(doc);
 
   await mongoose.disconnect();
 })();
