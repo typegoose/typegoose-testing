5168c7b197b8e1b9df58b58a7590c0aebe7e4e6e
hasezoey <hasezoey@gmail.com>
2020-09-06 10:18:02 +0200

    problem

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,37 @@
 // NodeJS: 14.9.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.3.5
+import { getModelForClass, getName, prop, setLogLevel } from "@typegoose/typegoose"; // @typegoose/typegoose@7.3.5
+import { schemas } from "@typegoose/typegoose/lib/internal/data";
 import * as mongoose from "mongoose"; // mongoose@5.10.3 @types/mongoose@5.7.36
 
-class User {
+setLogLevel("DEBUG");
+
+class Nested {
   @prop()
-  public username?: string;
+  public proper?: string;
 }
-const UserModel = getModelForClass(User);
+
+class ArrayClass {
+  @prop({ type: Nested, dim: 3, required: true })
+  public propy!: Nested[][][];
+}
+
+// class ArrayClass {
+//   @prop({ type: String, dim: 3, required: true })
+//   public propy!: string[][][];
+// }
+const ArrayModel = getModelForClass(ArrayClass);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
 
-  const doc = new UserModel({ username: "user1" });
+  const doc = new ArrayModel({ propy: [[["hi"]]] });
 
   console.log(doc);
+
+  console.log(JSON.stringify(schemas.get(getName(ArrayClass)), null, 2));
+
+  console.log(ArrayModel.schema.path("propy"));
 
   await mongoose.disconnect();
 })();
