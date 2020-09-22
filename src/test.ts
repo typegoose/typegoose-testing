a498fd596839cbb5ac49358980f142cc916171c3
hasezoey <hasezoey@gmail.com>
2020-09-22 18:23:49 +0200

    cant reproduce

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,15 @@
 // NodeJS: 14.10.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.4.0
+import { getModelForClass, pre, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.4.0
 import * as mongoose from "mongoose"; // mongoose@5.10.6 @types/mongoose@5.7.36
 
+async function somefunction() { // to have like in the example
+  return "t";
+}
+
+@pre<User>("save", async function () {
+  this.username = await somefunction();
+})
 class User {
   @prop()
   public username?: string;
