2b6ff43ae30b3a2695672b858dffe1ee03b5a242
hasezoey <hasezoey@gmail.com>
2020-12-02 12:27:55 +0100

    update deps

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
-// NodeJS: 14.15.0
+// NodeJS: 14.15.1
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.4.1
+import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.4.2
 import * as mongoose from "mongoose"; // mongoose@5.10.12 @types/mongoose@5.7.37
 
 class User {
