2c8e58f8b2d044d65137c0b104e877eafa466c89
hasezoey <hasezoey@gmail.com>
2021-04-23 11:23:24 +0200

    update to typegoose 8.0.0 beta

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 // NodeJS: 14.16.1
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.6.0
-import * as mongoose from "mongoose"; // mongoose@5.10.18 @types/mongoose@5.10.5
+import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@8.0.0-beta.5
+import * as mongoose from "mongoose"; // mongoose@5.12.5
 
 class User {
   @prop()
