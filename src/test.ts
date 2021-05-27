06945c4b5d2b00cc35fc95b1a8c40e3ddf5573ae
hasezoey <hasezoey@gmail.com>
2021-05-27 12:30:25 +0200

    cannot reproduce

--- src/test.ts
+++ src/test.ts
@@ -3,16 +3,41 @@
 import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.6.0
 import * as mongoose from "mongoose"; // mongoose@5.10.18 @types/mongoose@5.10.5
 
-class User {
-  @prop()
-  public username?: string;
+export class User {
+  public readonly _id?: mongoose.Types.ObjectId;
+
+  @prop({ required: true, type: () => Date })
+  public createdAt!: Date;
+
+  @prop({ type: () => Date })
+  public deletedAt!: Date | null;
+
+  @prop({ required: true })
+  public email!: string;
+
+  @prop({ required: true, type: () => Date })
+  public updatedAt!: Date;
+
+  @prop({ required: true })
+  public username!: string;
+
+  @prop({ required: true })
+  public password!: string;
 }
-const UserModel = getModelForClass(User);
+
+export const UserModel = getModelForClass(User);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
 
-  const doc = new UserModel({ username: "user1" });
+  const doc = await UserModel.create({
+    createdAt: new Date(),
+    deletedAt: null,
+    email: "y",
+    updatedAt: new Date(),
+    username: "y",
+    password: "y",
+  });
 
   console.log(doc);
 
