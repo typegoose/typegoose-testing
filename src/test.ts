a73ce44224c8be12be9047370a7f5141307cb723
hasezoey <hasezoey@gmail.com>
2021-06-15 10:30:30 +0200

    update mongoose version to 5.12.13

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 // NodeJS: 16.1.0
 // MongoDB: 4.2-bionic (Docker)
 import { getModelForClass, prop, Ref } from "@typegoose/typegoose"; // @typegoose/typegoose@8.0.0-beta.9
-import * as mongoose from "mongoose"; // mongoose@5.12.10
+import * as mongoose from "mongoose"; // mongoose@5.12.13
 import { inspect } from "util";
 
 // class DummyClass {
