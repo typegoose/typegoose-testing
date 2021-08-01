0ed1db7b3bdebbade095f99005a4e4f23c55d055
hasezoey <hasezoey@gmail.com>
2021-08-01 15:46:59 +0200

    finished for mongoose issue

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,56 @@
 // NodeJS: 16.6.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@8.0.1
+// import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@8.0.1
+// import { AnyParamConstructor, BeAnObject, DocumentType, ModelType, ReturnModelType } from "@typegoose/typegoose/lib/types";
 import * as mongoose from "mongoose"; // mongoose@5.13.5
 
-class User {
-  @prop()
-  public username?: string;
+// when the "AnyKeys" in mongoose is replaced with this, it works
+// type AnyKeys<T> = { [P in keyof T]?: T[P] | any };
+
+// original typegoose issue, cut from the mongoose issue
+// class User {
+//   @prop()
+//   public _id?: string;
+
+//   @prop()
+//   public test?: string;
+
+//   public static async ensure<U extends typeof User>(
+//     this: ModelType<InstanceType<U>>,
+//   ): Promise<mongoose.EnforceDocument<DocumentType<InstanceType<U>>, BeAnObject>> {
+//     const _id = "hello";
+//     const test = "hello too";
+//     await this.create({ test });
+
+//     const t: AnyKeys<InstanceType<U>> = { test };
+//     return new this({ test });
+//   }
+// }
+// const UserModel = getModelForClass(User);
+
+class Testy {
+  public prop1!: string;
+  public prop2?: string;
 }
-const UserModel = getModelForClass(User);
 
-(async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
+// the following 2 functions should be the same in types, so they both have the error
+function test1<U extends typeof Testy>(arg1: mongoose.Model<mongoose.Document & InstanceType<U>>) {
+  const t = new arg1({ prop2: "hello" }); // error here
+}
+function test2<U extends Testy>(arg1: mongoose.Model<mongoose.Document & U>) {
+  const t = new arg1({ prop2: "hello" }); // error here
+}
 
-  const doc = new UserModel({ username: "user1" });
+// working, both of these blocks should be equal in types
+const sch = new mongoose.Schema({ prop1: String, prop2: String });
+sch.loadClass(Testy);
+{
+  const model: mongoose.Model<mongoose.Document<any, any> & InstanceType<typeof Testy>> = mongoose.model("testy1", sch);
 
-  console.log(doc);
+  const t = new model({ prop2: "hello" });
+}
+{
+  const model: mongoose.Model<mongoose.Document<any, any> & Testy> = mongoose.model("testy2", sch);
 
-  await mongoose.disconnect();
-})();
+  const t = new model({ prop2: "hello" });
+}
