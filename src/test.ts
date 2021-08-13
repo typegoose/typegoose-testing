ab9411231bfcc650fd64a345ebccfb36b7a8a23e
hasezoey <hasezoey@gmail.com>
2021-08-13 02:05:14 +0200

    confirmed

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,76 @@
 // NodeJS: 16.6.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@8.1.1
+import { defaultClasses, getModelForClass, prop, ReturnModelType } from "@typegoose/typegoose"; // @typegoose/typegoose@8.1.1
+import { AnyParamConstructor, BeAnObject } from "@typegoose/typegoose/lib/types";
 import * as mongoose from "mongoose"; // mongoose@5.13.5
 
-class User {
-  @prop()
-  public username?: string;
+enum AudioModel {
+  A = "story-item",
+  B = "story-log-item",
+  C = "coach-comment",
+  D = "coach-summary"
 }
-const UserModel = getModelForClass(User);
+
+class AclBase implements defaultClasses.Base {
+  public _id!: mongoose.Types.ObjectId;
+  public id!: string;
+}
+
+class AudioRecordBase extends AclBase {
+  @prop()
+  public filePath?: string;
+
+  @prop()
+  public audio?: string;
+
+  public get hasAudio() {
+    return !!this.audio;
+  }
+}
+
+class StoryItem extends AudioRecordBase {
+  @prop()
+  public prompt: string;
+
+  @prop()
+  public header: string;
+}
+
+class QuestionItem extends AudioRecordBase {
+  @prop()
+  public question: string;
+
+  @prop()
+  public answer: string;
+}
+
+class ProposalItem extends AudioRecordBase {
+  @prop()
+  public person: string;
+
+  @prop()
+  public text: string;
+}
+
+class DataItem extends AudioRecordBase {
+  @prop()
+  public data: string;
+}
+
+const aItemModel = getModelForClass(StoryItem);
+const bItemModel = getModelForClass(QuestionItem);
+const cItemModel = getModelForClass(ProposalItem);
+const dItemModel = getModelForClass(DataItem);
+
+const audioModelMap1: Record<AudioModel, ReturnModelType<typeof AudioRecordBase, BeAnObject>> = {
+  [AudioModel.A]: aItemModel,
+  [AudioModel.B]: bItemModel,
+  [AudioModel.C]: cItemModel,
+  [AudioModel.D]: dItemModel,
+};
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
-
-  const doc = new UserModel({ username: "user1" });
-
-  console.log(doc);
 
   await mongoose.disconnect();
 })();
