1e5eb60d9d41b7f3dee20f71f887d4aca20f13f7
hasezoey <hasezoey@gmail.com>
2021-08-20 13:39:26 +0200

    update nodejs version

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-// NodeJS: 16.6.1
+// NodeJS: 16.7.0
 // MongoDB: 4.2-bionic (Docker)
 import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@8.1.1
 import * as mongoose from "mongoose"; // mongoose@5.13.5
