29800305aacff6e1d14071d2551fa78b59908f9d
hasezoey <hasezoey@gmail.com>
2021-08-20 14:00:58 +0200

    mongoose 6.0.0

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,20 @@
 // NodeJS: 16.7.0
 // MongoDB: 4.2-bionic (Docker)
-import * as mongoose from "mongoose"; // mongoose@5.13.7
+import * as mongoose from "mongoose"; // mongoose@6.0.0 14dd60be1f11969bcf7c84fe903ffe0471ff1485
 
 function validateOuter(...args: any[]): boolean {
-  console.log("validateOuter1", args.length, args[0].length, ...args); // expecting "validateOuter1 1 2 [{"_id":"611f9876cdb8e5cbede57261","dummyProp":"doc1","__v":0},{"_id":"611f9876cdb8e5cbede57262","dummyProp":"doc2","__v":0}]"
+  console.log("validateOuter1", args.length, args[0].length, ...args); // expecting "validateOuter1 1 2 [ new ObjectId("611f98f8ba650a92a093fd2b"), new ObjectId("611f98f8ba650a92a093fd2c") ]"
   console.log("validateOuter2", "_id" in args[0][0], "_id" in args[0][1]); // expecting "validateOuter2 true true"
 
   return true;
 }
 function validateInner(...args: any[]): boolean {
-  console.log("validateInner1", args.length, ...args); // expecting "validateInner1 1 { _id: 611f9876cdb8e5cbede57261, dummyProp: 'doc1', __v: 0 }" x2
-  console.log("validateInner2", "_id" in args[0]); // expecting "validateInner2 true" x2
+  console.log("validateInner1", args.length, ...args); // expecting "validateInner1 1 new ObjectId("611f98f8ba650a92a093fd2b")" x2
+  console.log("validateInner2", "_id" in args[0]); // expecting "validateInner2 false" x2
+
+  // but actual output in 6.0 is like in 5.13.7, populated
+  // "validateInner1 1 { dummyProp: 'doc1', _id: new ObjectId("611f98f8ba650a92a093fd2b"), __v: 0; }"
+  // "validateInner2 true"
 
   return true;
 }
@@ -33,7 +37,7 @@ const NestedModel = mongoose.model("Nested", NestedSchema);
 const MainModel = mongoose.model("Main", MainSchema);
 
 (async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
+  await mongoose.connect(`mongodb://localhost:27017/`, { dbName: "verifyMASTER" });
 
   const references = await NestedModel.create([{ dummyProp: "doc1" }, { dummyProp: "doc2" }]);
 
