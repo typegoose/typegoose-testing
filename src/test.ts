bc3daeb60773c4236a75dee4d3f401e95bf8b75e
hasezoey <hasezoey@gmail.com>
2021-08-20 13:56:53 +0200

    test in mongoose 5.13.7

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,45 @@
 // NodeJS: 16.7.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@8.1.1
-import * as mongoose from "mongoose"; // mongoose@5.13.5
+import * as mongoose from "mongoose"; // mongoose@5.13.7
 
-class User {
-  @prop()
-  public username?: string;
+function validateOuter(...args: any[]): boolean {
+  console.log("validateOuter1", args.length, args[0].length, ...args); // expecting "validateOuter1 1 2 [{"_id":"611f9876cdb8e5cbede57261","dummyProp":"doc1","__v":0},{"_id":"611f9876cdb8e5cbede57262","dummyProp":"doc2","__v":0}]"
+  console.log("validateOuter2", "_id" in args[0][0], "_id" in args[0][1]); // expecting "validateOuter2 true true"
+
+  return true;
 }
-const UserModel = getModelForClass(User);
+function validateInner(...args: any[]): boolean {
+  console.log("validateInner1", args.length, ...args); // expecting "validateInner1 1 { _id: 611f9876cdb8e5cbede57261, dummyProp: 'doc1', __v: 0 }" x2
+  console.log("validateInner2", "_id" in args[0]); // expecting "validateInner2 true" x2
+
+  return true;
+}
+
+const NestedSchema = new mongoose.Schema({
+  dummyProp: String,
+});
+const MainSchema = new mongoose.Schema({
+  refarray: {
+    validate: validateOuter,
+    type: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Nested",
+      validate: validateInner,
+    }]
+  }
+});
+
+const NestedModel = mongoose.model("Nested", NestedSchema);
+const MainModel = mongoose.model("Main", MainSchema);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
 
-  const doc = new UserModel({ username: "user1" });
+  const references = await NestedModel.create([{ dummyProp: "doc1" }, { dummyProp: "doc2" }]);
 
-  console.log(doc);
+  const main = await MainModel.create({ refarray: [...references] });
+
+  console.log("main", main);
 
   await mongoose.disconnect();
 })();
