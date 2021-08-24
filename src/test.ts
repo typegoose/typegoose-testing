13b19a8257a79971df0a23b70d210d5023b790b1
hasezoey <hasezoey@gmail.com>
2021-08-24 15:43:07 +0200

    replace tslint with eslint

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,24 @@
 // NodeJS: 16.7.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@8.1.1
-import * as mongoose from "mongoose"; // mongoose@5.13.5
+import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@8.1.1
+import * as mongoose from 'mongoose'; // mongoose@5.13.5
 
 class User {
   @prop()
   public username?: string;
 }
+
 const UserModel = getModelForClass(User);
 
 (async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
+  await mongoose.connect(`mongodb://localhost:27017/`, {
+    useNewUrlParser: true,
+    dbName: 'verifyMASTER',
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
 
-  const doc = new UserModel({ username: "user1" });
+  const doc = new UserModel({ username: 'user1' });
 
   console.log(doc);
 
