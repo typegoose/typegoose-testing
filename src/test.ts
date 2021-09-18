80ad222b762c59627e11d005d74fbdfaf165641d
hasezoey <hasezoey@gmail.com>
2021-09-18 20:02:21 +0200

    unclear error message

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,7 @@
-// NodeJS: 16.8.0
+// NodeJS: 16.9.1
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.0.0-beta.6
-import * as mongoose from 'mongoose'; // mongoose@6.0.5
+import * as mongoose from 'mongoose'; // mongoose@6.0.6
 
-class User {
-  @prop()
-  public username?: string;
-}
-
-const UserModel = getModelForClass(User);
-
-(async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, {
-    dbName: 'verifyMASTER',
-  });
-
-  const doc = new UserModel({ username: 'user1' });
-
-  console.log(doc);
-
-  await mongoose.disconnect();
-})();
+const sch = new mongoose.Schema({
+  something: [{ type: { somePath: String } }],
+});
