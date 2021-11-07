8d26618dcafc22ccee40ade855fe9fe2ab1120f7
hasezoey <hasezoey@gmail.com>
2021-11-07 15:47:33 +0100

    set to old/8.x

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 // NodeJS: 16.11.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@7.6.3
-import * as mongoose from 'mongoose'; // mongoose@5.10.18
+import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@8.3.0
+import * as mongoose from 'mongoose'; // mongoose@5.13.13
 
 class User {
   @prop()
