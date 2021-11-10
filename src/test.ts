fdfb0966946e8d02e48ae6ac4df79600e0b871a9
hasezoey <hasezoey@gmail.com>
2021-11-10 14:51:57 +0100

    working

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,103 @@
 // NodeJS: 16.11.0
 // MongoDB: 4.2-bionic (Docker)
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.2.0
+import { assertion } from '@typegoose/typegoose/lib/internal/utils';
 import * as mongoose from 'mongoose'; // mongoose@6.0.12
 
-class User {
-  @prop()
-  public username?: string;
-}
+mongoose.set('debug', true);
 
-const UserModel = getModelForClass(User);
+const UserSchema = new mongoose.Schema({
+  account: {
+    username: String,
+    email: String,
+    password: String,
+    twoFactorAuth: Boolean,
+    phone: String,
+    userImage: String,
+  },
+  // titles: {
+  //   status: String,
+  //   bio: String,
+  // },
+  // activity: {
+  //   joined: Date,
+  //   rank: [Number],
+  //   exp: Number,
+  //   seen: Date,
+  //   token: String,
+  //   lastLogin: Date,
+  // },
+  // friends: {
+  //   friendList: [Number],
+  //   friendRequests: [Number],
+  //   friendRequestsSent: [Number],
+  // },
+  // apiReqs: Number,
+  // imageApiReqs: Number,
+  // id: Number,
+});
+
+const UserModel = mongoose.model('user', UserSchema);
+
+console.log('path', UserModel.schema.path('account.username'));
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  await UserModel.collection.drop(); // clean collection
 
-  console.log(doc);
+  const initDocValid = await UserModel.create({
+    account: {
+      username: 'aaa',
+      email: 'aaa',
+      password: 'error',
+      twoFactorAuth: false,
+    },
+    // titles: {
+    //   status: "I'm new!",
+    // },
+    // activity: {
+    //   joined: Date.now(),
+    //   exp: 0,
+    //   seen: Date.now(),
+    //   token: 'someRandomHash',
+    //   lastLogin: Date.now(),
+    //   rank: [1],
+    // },
+    // friends: {
+    //   friendList: [1],
+    //   friendRequests: [2],
+    //   friendRequestsSent: [3],
+    // },
+    // apiReqs: 0,
+    // imageApiReqs: 0,
+    // id: 3,
+  });
+
+  const initDocNot = await UserModel.create({
+    account: {
+      username: 'hello',
+      email: 'hello',
+      password: 'error2',
+      twoFactorAuth: true,
+    },
+  });
+
+  // not findOne, to test what is actually given back throughout, not just one
+  const foundInvalid = await UserModel.find({ username: 'aaa' });
+
+  assertion(foundInvalid.length >= 2);
+
+  // not working
+  // const foundValid = await UserModel.find({ account: { username: 'aaa' } });
+  // working
+  const foundValid = await UserModel.find({ 'account.username': 'aaa' });
+
+  console.log(foundValid);
+  assertion(foundValid.length === 1);
+  assertion(foundValid[0].account.username === 'aaa');
 
   await mongoose.disconnect();
 })();
