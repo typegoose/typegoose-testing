03bf9c1570278e269607a31a31a80e8f1742ae89
hasezoey <hasezoey@gmail.com>
2021-11-20 00:59:28 +0100

    working

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,33 @@
 // NodeJS: 16.11.1
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.2.0
+import { DocumentType, getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.2.0
 import * as mongoose from 'mongoose'; // mongoose@6.0.12
 
-class User {
+class Child {
   @prop()
-  public username?: string;
+  public name?: string;
 }
 
-const UserModel = getModelForClass(User);
+class Parent {
+  @prop({ required: true, default: [], type: () => Child })
+  public children?: mongoose.Types.DocumentArray<DocumentType<Child>>;
+}
+
+const ParentModel = getModelForClass(Parent);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const doc = await ParentModel.create({});
 
-  console.log(doc);
+  console.log('before doc', doc);
+
+  const tmp = doc.children!.create({ name: 'test' });
+
+  console.log('generated subdoc', tmp);
+  console.log('after doc', doc);
 
   await mongoose.disconnect();
 })();
