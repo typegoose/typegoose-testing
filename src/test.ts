072941c42d9c0eb8ed588b7e757e4a8435c46c46
hasezoey <hasezoey@gmail.com>
2021-11-24 13:04:53 +0100

    pure mongoose reproduction code 6.x

--- src/test.ts
+++ src/test.ts
@@ -1,30 +1,52 @@
 // NodeJS: 16.11.1
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop, Ref, ReturnModelType } from '@typegoose/typegoose'; // @typegoose/typegoose@9.2.0
-import { AnyParamConstructor } from '@typegoose/typegoose/lib/types';
+// import { getModelForClass, prop, Ref, ReturnModelType } from '@typegoose/typegoose'; // @typegoose/typegoose@9.2.0
+// import { AnyParamConstructor } from '@typegoose/typegoose/lib/types';
 import * as mongoose from 'mongoose'; // mongoose@6.0.12
 
-class Nested {
-  @prop()
-  public nested?: string;
-}
+// class Nested {
+//   @prop()
+//   public nested?: string;
+// }
 
-const NestedModel = getModelForClass(Nested);
+// const NestedModel = getModelForClass(Nested);
 
-class Base {
-  @prop()
-  public something?: string;
+// class Base {
+//   @prop()
+//   public something?: string;
 
-  @prop({ ref: () => Nested })
-  public reference?: Ref<Nested>;
-}
+//   @prop({ ref: () => Nested })
+//   public reference?: Ref<Nested>;
+// }
 
-const BaseModel = getModelForClass(Base);
+// const BaseModel = getModelForClass(Base);
 
-function getModelForDb<T extends AnyParamConstructor<any>>(databaseName: string, model: ReturnModelType<T>): ReturnModelType<T> & T {
+// function getModelForDb<T extends AnyParamConstructor<any>>(databaseName: string, model: ReturnModelType<T>): ReturnModelType<T> & T {
+//   const db = mongoose.connection.useDb(databaseName);
+
+//   const DbModel = db.model(model.modelName, model.schema) as ReturnModelType<T> & T;
+
+//   return DbModel;
+// }
+
+const NestedSchema = new mongoose.Schema({ nested: { type: String } });
+const NestedModel = mongoose.model('Nested', NestedSchema);
+
+const BaseSchema = new mongoose.Schema({
+  something: {
+    type: String,
+  },
+  reference: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Nested',
+  },
+});
+const BaseModel = mongoose.model('Base', BaseSchema);
+
+function getModelForDb<T>(databaseName: string, model: mongoose.Model<T>): mongoose.Model<T> {
   const db = mongoose.connection.useDb(databaseName);
 
-  const DbModel = db.model(model.modelName, model.schema) as ReturnModelType<T> & T;
+  const DbModel = db.model(model.modelName, model.schema) as mongoose.Model<T>;
 
   return DbModel;
 }
@@ -34,21 +56,43 @@ function getModelForDb<T extends AnyParamConstructor<any>>(databaseName: string,
     dbName: 'verifyDefault',
   });
 
-  const nestedInDefault = await NestedModel.create({ nested: 'helloInDefault1' });
-  const baseInDefault = await BaseModel.create({ something: 'HelloInDefault2', nested: nestedInDefault });
+  // START SETUP CODE - run once without the code after it
 
-  console.log('nestedInDefault', nestedInDefault);
-  console.log('baseInDefault', baseInDefault);
+  {
+    const nestedInDefault = await NestedModel.create({ nested: 'helloInDefault1' });
+    const baseInDefault = await BaseModel.create({ something: 'HelloInDefault2', reference: nestedInDefault });
 
-  // purposefully in a different order
-  const cBaseModel = getModelForDb('verifyChanged', BaseModel);
-  const cNestedModel = getModelForDb('verifyChanged', NestedModel);
+    const cBaseModel = getModelForDb('verifyChanged', BaseModel);
+    const cNestedModel = getModelForDb('verifyChanged', NestedModel);
 
-  const nestedInChanged = await cNestedModel.create({ nested: 'helloInChanged1' });
-  const baseInChanged = await cBaseModel.create({ something: 'helloInChanged2', nested: nestedInChanged });
+    const nestedInChanged = await cNestedModel.create({
+      _id: new mongoose.Types.ObjectId('619cdac1ce8470ff0831ca1c'),
+      nested: 'helloInChanged1',
+    });
+    const baseInChanged = await cBaseModel.create({
+      something: 'helloInChanged2',
+      reference: new mongoose.Types.ObjectId('619cdac1ce8470ff0831ca1c'),
+    });
+  }
 
-  console.log('nestedInChanged', nestedInChanged);
-  console.log('baseInChanged', baseInChanged);
+  // END SETUP CODE
+
+  // Comment the setup code out after running once, and then comment the following code back in (not the double-commented out things)
+
+  // const cBaseModel = getModelForDb('verifyChanged', BaseModel);
+  // // const cNestedModel = getModelForDb('verifyChanged', NestedModel);
+
+  // const baseInChanged = await cBaseModel.create({
+  //   something: 'helloInChanged2',
+  //   reference: new mongoose.Types.ObjectId('619cdac1ce8470ff0831ca1c') /* nestedInChanged */,
+  // });
+
+  // // console.log('nestedInChanged', nestedInChanged);
+  // console.log('baseInChanged', baseInChanged);
+
+  // await baseInChanged.populate('reference'); // error here
+
+  // console.log('baseInChanged populated', baseInChanged);
 
   await mongoose.disconnect();
 })();
