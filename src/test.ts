5ca376704e014c8f956e5f49d8fc16c8cc214f5a
hasezoey <hasezoey@gmail.com>
2021-12-14 19:01:03 +0100

    mongoose 6.0.14

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,34 @@
 // NodeJS: 17.1.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.3.1
+// import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.3.1
 import * as mongoose from 'mongoose'; // mongoose@6.0.14
 
+// class User {
+//   @prop()
+//   public username?: string;
+// }
+
+// const UserModel = getModelForClass(User);
+
 class User {
-  @prop()
   public username?: string;
 }
 
-const UserModel = getModelForClass(User);
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+  },
+});
+
+const UserModel: mongoose.Model<User> = mongoose.model('User', UserSchema);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
-
-  console.log(doc);
+  const doc = await UserModel.create({ username: 'hello' });
+  await doc.updateOne(doc);
 
   await mongoose.disconnect();
 })();
