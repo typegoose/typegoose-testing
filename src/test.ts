e49118034c8bc6a3bfb4264497e38ce4aaa6d90a
hasezoey <hasezoey@gmail.com>
2022-02-22 10:11:09 +0100

    upgrade typegoose and mongoose

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 // NodeJS: 17.3.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.5.0
-import * as mongoose from 'mongoose'; // mongoose@6.1.8
+import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.6.2
+import * as mongoose from 'mongoose'; // mongoose@6.2.2
 
 class User {
   @prop()
