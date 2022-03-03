32d7b18ded0643634c96f337e4aa06eefa101de6
hasezoey <hasezoey@gmail.com>
2022-03-03 13:11:42 +0100

    minify for esm

--- src/test.ts
+++ src/test.ts
@@ -1,40 +1,21 @@
 // NodeJS: 17.6.0
 // MongoDB: 4.2-bionic (Docker)
-import { prop, isDocument, Ref, DocumentType, getModelForClass } from '@typegoose/typegoose'; // @typegoose/typegoose@9.7.0
+import { prop, getModelForClass } from '@typegoose/typegoose'; // @typegoose/typegoose@9.7.0
 import mongoose from 'mongoose'; // mongoose@6.2.4
 
-class Dog {
+class User {
   @prop()
   public name?: string;
 }
 
-class Cat {
-  @prop({ ref: () => Dog })
-  public partner?: Ref<Dog>;
-
-  public async hasPartner(this: DocumentType<Cat>) {
-    await this.populate('partner');
-
-    if (isDocument(this.partner)) {
-      console.log(this.partner.name);
-    }
-  }
-}
-
-const DogModel = getModelForClass(Dog);
-const CatModel = getModelForClass(Cat);
+const UserModel = getModelForClass(User);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const dog1 = await DogModel.create({ name: 'hello' });
-  const cat1 = await CatModel.create({ partner: dog1._id });
-
-  const found = await CatModel.findById(cat1._id).orFail().exec();
-
-  await found.hasPartner();
+  const cat1 = await UserModel.create({ name: "hello" });
 
   await await mongoose.disconnect();
 })();
