4961f1425cee41dd916281809941dd2bfa4eb5c1
hasezoey <hasezoey@gmail.com>
2022-03-03 11:14:12 +0100

    cannot reproduce

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,40 @@
 // NodeJS: 17.6.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.7.0
+import { DocumentType, getModelForClass, isDocument, prop, Ref } from '@typegoose/typegoose'; // @typegoose/typegoose@9.7.0
 import * as mongoose from 'mongoose'; // mongoose@6.2.4
 
-class User {
+class Dog {
   @prop()
-  public username?: string;
+  public name?: string;
 }
 
-const UserModel = getModelForClass(User);
+class Cat {
+  @prop({ ref: () => Dog })
+  public partner?: Ref<Dog>;
+
+  public async hasPartner(this: DocumentType<Cat>) {
+    await this.populate('partner');
+
+    if (isDocument(this.partner)) {
+      console.log(this.partner.name);
+    }
+  }
+}
+
+const DogModel = getModelForClass(Dog);
+const CatModel = getModelForClass(Cat);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const dog1 = await DogModel.create({ name: 'hello' });
+  const cat1 = await CatModel.create({ partner: dog1._id });
 
-  console.log(doc);
+  const found = await CatModel.findById(cat1._id).orFail().exec();
 
-  await mongoose.disconnect();
+  await found.hasPartner();
+
+  await await mongoose.disconnect();
 })();
