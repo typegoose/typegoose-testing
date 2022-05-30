cb65ab9458b2085be5b26eecdc85073b146be381
hasezoey <hasezoey@gmail.com>
2022-05-31 01:34:38 +0200

    update nodejs and mongodb (server) version

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
-// NodeJS: 17.8.0
-// MongoDB: 4.2-bionic (Docker)
+// NodeJS: 18.2.0
+// MongoDB: 5.0 (Docker)
 // Typescript 4.5.5
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.8.0
 import * as mongoose from 'mongoose'; // mongoose@6.3.0
