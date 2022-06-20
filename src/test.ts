fc31453a5463f03d434fb1459dfb26ce4a770bbb
hasezoey <hasezoey@gmail.com>
2022-06-20 13:02:55 +0200

    mongoose 6.4.0

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@
 // Typescript 4.7.2
 // import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.9.0
 // import { BeAnObject, DocumentType } from '@typegoose/typegoose/lib/types';
-import * as mongoose from 'mongoose'; // mongoose@6.3.9
+import * as mongoose from 'mongoose'; // mongoose@6.4.0
 
 // type SubDocumentType<T, QueryHelpers = BeAnObject> = DocumentType<T, QueryHelpers> & mongoose.Types.Subdocument;
 // type ArraySubDocumentType<T, QueryHelpers = BeAnObject> = DocumentType<T, QueryHelpers> & mongoose.Types.ArraySubdocument;
@@ -69,13 +69,13 @@ const ParentModel = mongoose.model<DocumentType<Parent>>('Parent', ParentSchema)
     nestedArray: [{ dummy: 'hello again' }],
   });
 
-  doc.username; // expected type: "string | undefined"
-  doc.map; // expected type: "Map<string, string> | undefined"
-  const t = doc.map!.get('key1'); // expected type: "string | undefined"
-  doc.nested; // expected type: "SubDocumentType<Nested> | undefined"
-  doc.nested!.parent(); // expecting function to exist
-  doc.nestedArray; // expected type: "ArraySubDocumentType<Nested>[] | undefined"
-  doc.nestedArray?.[0].parentArray(); // expecting function to exist
+  doc.username; // expected type: "string | undefined", actual: "string"
+  doc.map; // expected type: "Map<string, string> | undefined", actual: (explicit object)"{ key1: 'value1', key2: 'value2' }"
+  const t = doc.map!.get('key1'); // expected type: "string | undefined", actual: function "get" does not exist
+  doc.nested; // expected type: "SubDocumentType<Nested> | undefined", actual: (explicit object)"{ dummy: 'hello' }"
+  doc.nested!.parent(); // expecting function to exist, actual: function "parent" does not exist
+  doc.nestedArray; // expected type: "ArraySubDocumentType<Nested>[] | undefined", actual: (explicit object array)"[{ dummy: 'hello again' }]"
+  doc.nestedArray?.[0].parentArray(); // expecting function to exist, actual: function "parentArray" does not exist
 
   await mongoose.disconnect();
 })();
