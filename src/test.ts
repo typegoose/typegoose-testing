88e22ce2161b9d2bfb7efd00818601bdaa6af925
hasezoey <hasezoey@gmail.com>
2022-06-20 16:53:04 +0200

    cannot reproduce

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,48 @@
 // NodeJS: 18.3.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.7.2
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.9.0
+import { getDiscriminatorModelForClass, getModelForClass, modelOptions, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.9.0
 import * as mongoose from 'mongoose'; // mongoose@6.3.5
 
-class User {
-  @prop()
-  public username?: string;
+enum Kinds {
+  EXTEND1 = 'extend1',
+  EXTEND2 = 'extend2',
 }
 
-const UserModel = getModelForClass(User);
+@modelOptions({ schemaOptions: { discriminatorKey: 'kind' } })
+class Main {
+  @prop()
+  public shared?: string;
+
+  @prop({ required: true, enum: Kinds })
+  public kind!: string;
+}
+
+class Extend1 extends Main {
+  @prop()
+  public extend1?: string;
+}
+
+class Extend2 extends Main {
+  @prop()
+  public extend2?: string;
+}
+
+const MainModel = getModelForClass(Main);
+const Extend1Model = getDiscriminatorModelForClass(MainModel, Extend1, Kinds.EXTEND1);
+const Extend2Model = getDiscriminatorModelForClass(MainModel, Extend2, Kinds.EXTEND2);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
-    dbName: 'verifyMASTER',
+    dbName: 'discordAddamsson',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const extend1doc = await Extend1Model.create({ shared: '1', extend1: 'hello1' });
+  const extend2doc = await Extend2Model.create({ shared: '2', extend2: 'hello2' });
 
-  console.log(doc);
+  const foundDocs = await MainModel.find().exec();
+
+  console.log('found', foundDocs);
 
   await mongoose.disconnect();
 })();
