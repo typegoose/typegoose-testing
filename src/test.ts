8546b8488347267e08c29bfab189ecf390e09b79
hasezoey <hasezoey@gmail.com>
2022-07-04 13:52:56 +0200

    update deps

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 // NodeJS: 18.3.0
 // MongoDB: 5.0 (Docker)
-// Typescript 4.7.2
+// Typescript 4.7.4
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.10.1
 import * as mongoose from 'mongoose'; // mongoose@6.4.2
 
