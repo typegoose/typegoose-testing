6c2d995e3960d358de0e681e5846d0c58da9facd
hasezoey <hasezoey@gmail.com>
2022-08-05 12:54:30 +0200

    update mongoose and @typegoose/auto-increment

--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,7 @@
 // MongoDB: 5.0 (Docker)
 // Typescript 4.7.4
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.11.0
-import * as mongoose from 'mongoose'; // mongoose@6.5.0
+import * as mongoose from 'mongoose'; // mongoose@6.5.1
 
 class User {
   @prop()
