3a53542c1c2d160715582f8e31c041f50ea732db
hasezoey <hasezoey@gmail.com>
2022-08-26 12:27:59 +0200

    repro

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,28 @@
 // NodeJS: 18.7.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.7.4
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.11.0
 import * as mongoose from 'mongoose'; // mongoose@6.5.2
 
-class User {
-  @prop()
-  public username?: string;
-}
+const userSchema = new mongoose.Schema({ prop1: String });
 
-const UserModel = getModelForClass(User);
+const UserModel = mongoose.model('User', userSchema);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  mongoose.set('debug', true);
 
-  console.log(doc);
+  // Mongoose: users.remove({}, {})
+  // remove { acknowledged: true, deletedCount: 0 }
+  console.log('remove', await UserModel.remove({ nonExistent: true }));
+  // Mongoose: users.deleteOne({}, {})
+  // deleteOne { acknowledged: true, deletedCount: 0 }
+  console.log('deleteOne', await UserModel.deleteOne({ nonExistent: true }));
+  // Mongoose: users.deleteMany({}, {})
+  // deleteMany { acknowledged: true, deletedCount: 0 }
+  console.log('deleteMany', await UserModel.deleteMany({ nonExistent: true }));
 
   await mongoose.disconnect();
 })();
