cdee367bfb163473d971315f53ae0e74590fb5de
hasezoey <hasezoey@gmail.com>
2022-09-16 10:25:24 +0200

    cannot reproduce

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,39 @@
 // NodeJS: 18.7.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.8.2
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.11.2
-import * as mongoose from 'mongoose'; // mongoose@6.5.3
+import * as mongoose from 'mongoose'; // mongoose@6.3.5
 
-class User {
-  @prop()
-  public username?: string;
-}
+// Test #1
+// mongoose.set('toObject', { virtuals: true });
 
-const UserModel = getModelForClass(User);
+const schema = new mongoose.Schema(
+  {
+    prop1: String,
+  },
+  {
+    // Test #2
+    // toObject: {
+    //   virtuals: true,
+    // },
+  }
+);
+
+// the virtual to test
+schema.virtual('test').get(() => 1);
+
+const model = mongoose.model('Test', schema);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const doc = new model({ username: 'user1' });
 
-  console.log(doc);
+  // in 6.3.5 this always outputs the correct setting
+  console.log(doc.toObject());
+  // the following always works and is used a control
+  console.log(doc.toObject({ virtuals: true }));
 
   await mongoose.disconnect();
 })();
