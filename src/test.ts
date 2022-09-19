b667a084e95a5e6c33c248c3dd773587965d4a86
hasezoey <hasezoey@gmail.com>
2022-09-19 11:36:52 +0200

    MMS ChangeStreams

--- src/test.ts
+++ src/test.ts
@@ -3,22 +3,57 @@
 // Typescript 4.8.3
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.12.0
 import * as mongoose from 'mongoose'; // mongoose@6.6.1
+import { MongoMemoryReplSet } from 'mongodb-memory-server';
 
 class User {
   @prop()
   public username?: string;
+
+  @prop()
+  public someCount?: number;
 }
 
 const UserModel = getModelForClass(User);
 
 (async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, {
+  const instance = await MongoMemoryReplSet.create({
+    replSet: {
+      count: 3,
+      storageEngine: 'wiredTiger',
+    },
+  });
+
+  console.log('ReplSet started');
+
+  await mongoose.connect(instance.getUri(), {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  console.log('mongoose connected');
 
-  console.log(doc);
+  UserModel.watch().on('change', (doc) => {
+    console.log('CHANGE');
+  });
+
+  const doc = await UserModel.create({ username: 'hello', someCount: 0 });
+
+  console.log('running while');
+
+  let times = 10;
+
+  while (times > 0) {
+    await sleep(1000);
+
+    await UserModel.findOneAndUpdate({ _id: doc._id }, { $inc: { someCount: 1 } });
+    times -= 1;
+  }
+
+  console.log('stopping');
 
   await mongoose.disconnect();
+  await instance.stop();
 })();
+
+function sleep(time: number) {
+  return new Promise((res) => setTimeout(res, time));
+}
