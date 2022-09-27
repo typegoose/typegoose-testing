1ae1667e00317f4ac0c2d346f17b05f1d310be39
hasezoey <hasezoey@gmail.com>
2022-09-27 13:07:44 +0200

    confirm

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,41 @@
 // NodeJS: 18.8.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.8.3
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.12.0
 import * as mongoose from 'mongoose'; // mongoose@6.6.1
 
-class User {
-  @prop()
-  public username?: string;
-}
+const userSchema = new mongoose.Schema(
+  {
+    uuid: String,
+    addresses: [
+      new mongoose.Schema(
+        {
+          location: String,
+        },
+        { timestamps: true }
+      ),
+    ],
+  },
+  { timestamps: true }
+);
 
-const UserModel = getModelForClass(User);
+const User = mongoose.model('User', userSchema);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  let uuid = 'saveOp';
+  const home = { location: 'earth' };
 
-  console.log(doc);
+  const example = new User({ uuid, addresses: [home] });
+  mongoose.set('debug', true);
+  await example.save();
+
+  uuid = 'upsertOp';
+
+  const query = User.findOneAndUpdate({ uuid }, { uuid, $push: { addresses: home } }, { upsert: true, new: true, runValidators: true });
+  await query;
 
   await mongoose.disconnect();
 })();
