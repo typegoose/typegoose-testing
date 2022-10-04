18e9b188cd59b75c1652113930b73ad644078da4
hasezoey <hasezoey@gmail.com>
2022-10-04 18:49:32 +0200

    reproduce

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,37 @@
 // NodeJS: 18.8.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.8.4
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.12.1
+// import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.12.1
 import * as mongoose from 'mongoose'; // mongoose@6.6.3
 
-class User {
-  @prop()
-  public username?: string;
-}
+mongoose.set('strict', 'throw');
 
-const UserModel = getModelForClass(User);
+const options = { discriminatorKey: 'kind' };
+
+const EventSchema = new mongoose.Schema({ name: String, kind: String, animals: { kind: String, world: String } }, options);
+
+const Event = mongoose.model('Event', EventSchema);
+
+const ClickedLinkEvent = Event.discriminator('ClickedLink', new mongoose.Schema({ url: String }, options));
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const eventDoc = await Event.findByIdAndUpdate(
+    { _id: new mongoose.Types.ObjectId() },
+    {
+      $set: {
+        name: 'name',
+        // error "Can't modify discriminator key "kind" on discriminator model", even though its a different "kind"
+        animals: { kind: 'kind', world: 'world' },
+      },
+    },
+    {}
+  );
 
-  console.log(doc);
+  console.log(eventDoc);
 
   await mongoose.disconnect();
 })();
