68177532c0c512b05d1243b878532b1df7f22331
hasezoey <hasezoey@gmail.com>
2022-11-30 12:14:12 +0100

    fix

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,48 @@
 // NodeJS: 18.10.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.8.4
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@9.13.0
+import { getModelForClass, Prop, prop, PropType, Ref } from '@typegoose/typegoose'; // @typegoose/typegoose@9.13.0
 import * as mongoose from 'mongoose'; // mongoose@6.7.3
 
 class User {
-  @prop()
-  public username?: string;
+  @Prop()
+  name?: string;
+}
+
+class Exam {
+  @Prop()
+  name?: string;
+
+  @Prop({ ref: () => Exam }, PropType.ARRAY)
+  examReq?: Ref<Exam>[];
+}
+
+class UserExam {
+  @Prop({ ref: () => User })
+  userId: Ref<User>;
+
+  @Prop({ ref: () => Exam })
+  examId: Ref<Exam>;
 }
 
 const UserModel = getModelForClass(User);
+const ExamModel = getModelForClass(Exam);
+const UserExamModel = getModelForClass(UserExam);
 
 (async () => {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const baseExam = await ExamModel.create({ name: 'base' });
+  const refExam = await ExamModel.create({ name: 'ref', examReq: baseExam });
+  const user = await UserModel.create({ name: 'user' });
+  const userExam = await UserExamModel.create({ userId: user, examId: refExam });
 
-  console.log(doc);
+  mongoose.set('debug', true);
+  const found = await UserExamModel.findById(userExam).populate(/* ('examId.examReq') */ { path: 'examId', populate: { path: 'examReq' } });
+
+  console.log('found', found);
 
   await mongoose.disconnect();
 })();
