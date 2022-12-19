cfb45d17094df38dc4d5024caf2f109963c848e2
hasezoey <hasezoey@gmail.com>
2022-12-19 14:07:39 +0100

    reproduce

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,17 @@
 // NodeJS: 18.10.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.4
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@10.0.0
-import * as mongoose from 'mongoose'; // mongoose@6.8.0
 
-class User {
-  @prop()
-  public username?: string;
+interface Test {
+  testy?: string;
 }
 
-const UserModel = getModelForClass(User);
+interface Options {
+  test: Test['testy'];
+  // no error when doing the following
+  // test: string;
+  // or
+  // test?: string;
+}
 
-(async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, {
-    dbName: 'verifyMASTER',
-  });
-
-  const doc = new UserModel({ username: 'user1' });
-
-  console.log(doc);
-
-  await mongoose.disconnect();
-})();
+declare function includesAllRequiredOptions(options: Partial<Options>): options is Options;
