31d6d5858f298fc27f2c3199d54721f6d01f71e1
hasezoey <hasezoey@gmail.com>
2023-03-06 10:27:21 +0100

    upgrade to mongoose 7
    and typegoose 11 beta

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,8 @@
 // NodeJS: 18.10.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@10.2.0
-import * as mongoose from 'mongoose'; // mongoose@6.10.0
+import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.0.0-beta.1
+import * as mongoose from 'mongoose'; // mongoose@7.0.0
 
 class User {
   @prop()
