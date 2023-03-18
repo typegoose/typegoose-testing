bf2e392b5c9b244cf1c304a3382bdb668466a7dc
hasezoey <hasezoey@gmail.com>
2023-03-18 11:57:28 +0100

    repro

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,40 @@
 // NodeJS: 18.10.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@10.3.2
+import { getModelForClass, modelOptions, prop, Ref } from '@typegoose/typegoose'; // @typegoose/typegoose@10.3.2
 import * as mongoose from 'mongoose'; // mongoose@6.10.1
+
+@modelOptions({
+  options: {
+    customName: 'brand',
+  },
+})
+class Brand {
+  @prop({})
+  public name?: string;
+}
+
+class Car {
+  @prop()
+  name?: string;
+
+  @prop()
+  type?: 'SUV' | 'Sedan';
+
+  @prop({ ref: () => Brand })
+  brand?: Ref<Brand>;
+}
 
 class User {
   @prop()
-  public username?: string;
+  name?: string;
+
+  // @prop({ type: () => Car })
+  @prop({})
+  cars?: Car[];
 }
 
+const BrandModel = getModelForClass(Brand);
 const UserModel = getModelForClass(User);
 
 (async () => {
@@ -16,9 +42,40 @@ const UserModel = getModelForClass(User);
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const initBrand = await BrandModel.create([
+    {
+      // _id: '640575efb4187442b1e99bb9',
+      name: 'BMW',
+    },
+    {
+      // _id: '640575efb4187442b1eddddd',
+      name: 'Audi',
+    },
+  ]);
 
-  console.log(doc);
+  await UserModel.create({
+    name: '张三',
+    cars: [
+      {
+        name: 'X5',
+        type: 'SUV',
+        brand: initBrand[0]._id, //new Types.ObjectId('640575efb4187442b1e99bb9')
+      },
+      {
+        name: 'A6',
+        type: 'Sedan',
+        brand: initBrand[1]._id,
+      },
+    ],
+  });
+
+  const doc = await UserModel.findOne().orFail();
+
+  const data = await doc.populate('cars.brand');
+  //  const data = await u.populate({ path: "cars.brand", model: BrandModel });
+
+  // no "cars.brand" field
+  console.log('data is ', JSON.stringify(data, null, 2));
 
   await mongoose.disconnect();
 })();
