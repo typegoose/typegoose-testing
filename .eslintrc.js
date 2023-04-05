1280fdd600828e5d104e7ac7261801ba45f45b7e
hasezoey <hasezoey@gmail.com>
2023-04-05 11:17:46 +0200

    example for eslint-plugin-markdown

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,34 @@
 const path = require('path');
 
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    sourceType: 'module',
-    useJSXTextNode: true,
-    project: [
-      path.resolve(__dirname, 'tsconfig.json'),
-    ],
+    ecmaVersion: 2020,
   },
+  plugins: ['markdown'],
+  overrides: [
+    // eslint-plugin-markdown has been disabled because of out-standing issues, see https://github.com/eslint/eslint-plugin-markdown/issues/214
+    {
+      files: ['**/*.md'],
+      processor: 'markdown/markdown',
+    },
+    {
+      files: ['**/*.md/*.js', '**/*.md/*.javascript'],
+      parserOptions: {
+        ecmaFeatures: {
+          impliedStrict: true,
+        },
+        sourceType: 'module', // required to allow "import" statements
+        ecmaVersion: 'latest', // required to allow top-level await
+      },
+      rules: {
+        'no-undef': 'off',
+        'no-unused-expressions': 'off',
+        'no-unused-vars': 'off',
+        'no-redeclare': 'off',
+      },
+    },
+  ],
   rules: {
     'no-underscore-dangle': 0,
     'arrow-body-style': 0,
@@ -35,45 +50,55 @@ module.exports = {
     'prefer-destructuring': 0,
     'no-else-return': 1,
     'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-inferrable-types': 0,
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/no-use-before-define': 0,
-    '@typescript-eslint/no-empty-function': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    'curly': ['error', 'all'],
+    curly: ['error', 'all'],
     'padding-line-between-statements': [
-      "warn",
+      'warn',
       {
-        blankLine: 'always', prev: '*', next: 'return' // add blank line *before* all returns (if there are statements before)
+        blankLine: 'always',
+        prev: '*',
+        next: 'return', // add blank line *before* all returns (if there are statements before)
       },
       {
-        blankLine: 'always', prev: '*', next: 'if' // add blank line *before* all ifs
+        blankLine: 'always',
+        prev: '*',
+        next: 'if', // add blank line *before* all ifs
       },
       {
-        blankLine: 'always', prev: 'if', next: '*' // add blank line *after* all ifs
+        blankLine: 'always',
+        prev: 'if',
+        next: '*', // add blank line *after* all ifs
       },
       {
-        blankLine: 'any', prev: 'if', next: 'if' // allow blank line between ifs, but not enforce either
+        blankLine: 'any',
+        prev: 'if',
+        next: 'if', // allow blank line between ifs, but not enforce either
       },
       {
-        blankLine: 'always', prev: '*', next: ['function', 'class'] // add blank line *before* all functions and classes
+        blankLine: 'always',
+        prev: '*',
+        next: ['function', 'class'], // add blank line *before* all functions and classes
       },
       {
-        blankLine: 'always', prev: ['function', 'class'], next: '*' // add blank line *after* all functions and classes
+        blankLine: 'always',
+        prev: ['function', 'class'],
+        next: '*', // add blank line *after* all functions and classes
       },
       {
-        blankLine: 'always', prev: '*', next: 'import' // add blank line *before* all imports
+        blankLine: 'always',
+        prev: '*',
+        next: 'import', // add blank line *before* all imports
       },
       {
-        blankLine: 'always', prev: 'import', next: '*' // add blank line *after* all imports
+        blankLine: 'always',
+        prev: 'import',
+        next: '*', // add blank line *after* all imports
       },
       {
-        blankLine: 'never', prev: 'import', next: 'import' // dont allow blank line between imports
+        blankLine: 'never',
+        prev: 'import',
+        next: 'import', // dont allow blank line between imports
       },
-    ]
+    ],
   },
   env: {
     node: true,
---
+++ src/eslint_test.md
@@ -0,0 +1,20 @@
+test
+
+```js
+// file a
+const t = 'hello';
+export default t;
+
+// file b
+const t = 'something else';
+
+// file c
+const fileA = require('a');
+const fileB = require('b');
+
+const t = pkg.someFunction();
+t.someOtherFunction();
+// can also be written
+const t = pkg.someFunction().someOtherFunction();
+
+```
