d3173d3a76fa324ba1faaf51adf91d557ef21622
hasezoey <hasezoey@gmail.com>
2023-05-03 12:34:11 +0200

    reproduce mongoose

--- src/test.ts
+++ src/test.ts
@@ -1,17 +1,31 @@
 // NodeJS: 18.10.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.0.2
+// import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.0.2
 import { assertion } from '@typegoose/typegoose/lib/internal/utils';
 import * as mongoose from 'mongoose'; // mongoose@7.0.5
 import { inspect } from 'util';
 
-@modelOptions({})
-class TestingMultiDimArrays {
-  @prop({ type: Number, items: Number, dim: 2 })
-  public intArray?: number[][];
-}
-const TestingMultiDimArraysModel = getModelForClass(TestingMultiDimArrays);
+// @modelOptions({})
+// class TestingMultiDimArrays {
+//   @prop({ type: Number, items: Number, dim: 2 })
+//   public intArray?: number[][];
+// }
+// const TestingMultiDimArraysModel = getModelForClass(TestingMultiDimArrays);
+
+const schema = new mongoose.Schema({
+  intArray: {
+    type: [
+      [
+        {
+          type: Number,
+        },
+      ],
+    ],
+  },
+});
+
+const TestingMultiDimArraysModel = mongoose.model('TestingMultiDimArraysModel', schema);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
