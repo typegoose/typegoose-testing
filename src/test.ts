a5fa2191bad70df23b90dcc57299e88452dbc4ca
hasezoey <hasezoey@gmail.com>
2023-05-17 11:52:46 +0200

    repro mongoose

--- src/test.ts
+++ src/test.ts
@@ -1,77 +1,145 @@
 // NodeJS: 19.9.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, modelOptions, prop, Ref } from '@typegoose/typegoose'; // @typegoose/typegoose@11.1.0
+// import { getModelForClass, modelOptions, prop, Ref } from '@typegoose/typegoose'; // @typegoose/typegoose@11.1.0
 import * as mongoose from 'mongoose'; // mongoose@7.1.1
 
-class Book {
-  @prop({ required: true })
-  name!: string;
-}
-
-@modelOptions({ schemaOptions: { toJSON: { virtuals: true }, toObject: { virtuals: true } } })
-class NestedNested {
-  @prop()
-  bookId?: mongoose.Schema.Types.ObjectId;
-
-  @prop({
-    localField: 'bookId',
-    foreignField: '_id',
-    justOne: true,
-    ref: () => Book,
-  })
-  readonly book?: Ref<Book>;
-}
-
-@modelOptions({ schemaOptions: { toJSON: { virtuals: true }, toObject: { virtuals: true } } })
-class Nested {
-  @prop()
-  a?: number;
-
-  @prop()
-  bookId?: mongoose.Schema.Types.ObjectId;
-
-  @prop({
-    localField: 'bookId',
-    foreignField: '_id',
-    justOne: true,
-    ref: () => Book,
-  })
-  readonly book?: Ref<Book>;
-
-  @prop({ type: () => [NestedNested] })
-  nestedNested?: NestedNested[];
-}
-
-class Teacher {
-  @prop({ type: () => [mongoose.Schema.Types.ObjectId] })
-  bookIds?: mongoose.Schema.Types.ObjectId[];
-
-  @prop({
-    localField: 'bookIds',
-    foreignField: '_id',
-    ref: () => Book,
-  })
-  readonly books?: Ref<Book>[];
-
-  @prop({ type: Nested })
-  nested?: Nested;
-}
-
-const BookModel = getModelForClass(Book, {
-  schemaOptions: {
+const bookSchema = new mongoose.Schema(
+  { name: String },
+  {
     collection: 'books',
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-  },
-});
+  }
+);
 
-const TeacherModel = getModelForClass(Teacher, {
-  schemaOptions: {
+const BookModel = mongoose.model('Book', bookSchema);
+
+const nestedNestedSchema = new mongoose.Schema(
+  {
+    bookId: mongoose.Schema.Types.ObjectId,
+  },
+  {
+    virtuals: {
+      book: {
+        options: {
+          localField: 'bookId',
+          foreignField: '_id',
+          ref: 'Book',
+          justOne: true,
+        },
+      },
+    },
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
+  }
+);
+
+const nestedSchema = new mongoose.Schema(
+  {
+    a: Number,
+    bookId: mongoose.Schema.Types.ObjectId,
+
+    nestedNested: [nestedNestedSchema],
   },
-});
+  {
+    virtuals: {
+      book: {
+        options: { localField: 'bookId', foreignField: '_id', justOne: true, ref: 'Book' },
+      },
+    },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const teacherSchema = new mongoose.Schema(
+  {
+    bookIds: [mongoose.Schema.Types.ObjectId],
+
+    nested: nestedSchema,
+  },
+  {
+    virtuals: {
+      books: {
+        options: { localField: 'bookIds', foreignField: '_id', ref: 'Book' },
+      },
+    },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const TeacherModel = mongoose.model('Teacher', teacherSchema);
+
+// class Book {
+//   @prop({ required: true })
+//   name!: string;
+// }
+
+// @modelOptions({ schemaOptions: { toJSON: { virtuals: true }, toObject: { virtuals: true } } })
+// class NestedNested {
+//   @prop()
+//   bookId?: mongoose.Schema.Types.ObjectId;
+
+//   @prop({
+//     localField: 'bookId',
+//     foreignField: '_id',
+//     justOne: true,
+//     ref: () => Book,
+//   })
+//   readonly book?: Ref<Book>;
+// }
+
+// @modelOptions({ schemaOptions: { toJSON: { virtuals: true }, toObject: { virtuals: true } } })
+// class Nested {
+//   @prop()
+//   a?: number;
+
+//   @prop()
+//   bookId?: mongoose.Schema.Types.ObjectId;
+
+//   @prop({
+//     localField: 'bookId',
+//     foreignField: '_id',
+//     justOne: true,
+//     ref: () => Book,
+//   })
+//   readonly book?: Ref<Book>;
+
+//   @prop({ type: () => [NestedNested] })
+//   nestedNested?: NestedNested[];
+// }
+
+// class Teacher {
+//   @prop({ type: () => [mongoose.Schema.Types.ObjectId] })
+//   bookIds?: mongoose.Schema.Types.ObjectId[];
+
+//   @prop({
+//     localField: 'bookIds',
+//     foreignField: '_id',
+//     ref: () => Book,
+//   })
+//   readonly books?: Ref<Book>[];
+
+//   @prop({ type: Nested })
+//   nested?: Nested;
+// }
+
+// const BookModel = getModelForClass(Book, {
+//   schemaOptions: {
+//     collection: 'books',
+//     toJSON: { virtuals: true },
+//     toObject: { virtuals: true },
+//   },
+// });
+
+// const TeacherModel = getModelForClass(Teacher, {
+//   schemaOptions: {
+//     toJSON: { virtuals: true },
+//     toObject: { virtuals: true },
+//   },
+// });
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
