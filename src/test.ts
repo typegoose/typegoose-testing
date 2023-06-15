3bdf665c3bbec55060083df3276321eea5071efc
hasezoey <hasezoey@gmail.com>
2023-06-15 22:13:52 +0200

    cannot reproduce

--- src/test.ts
+++ src/test.ts
@@ -2,12 +2,30 @@
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.2.0
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import * as mongoose from 'mongoose'; // mongoose@7.2.1
 
-class User {
+/// ---------------------
+// Example from the documentation via the dual interface & class way
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+// interface User extends Base {}
+
+// class User extends TimeStamps {
+//   @prop()
+//   public username?: string;
+// }
+/// ---------------------
+
+/// ---------------------
+// Example via the "implements" way
+class User extends TimeStamps implements Base {
+  _id!: mongoose.Types.ObjectId;
+  id!: string;
   @prop()
   public username?: string;
 }
+
+/// ---------------------
 
 const UserModel = getModelForClass(User);
 
@@ -16,9 +34,14 @@ async function main() {
     dbName: 'verifyMASTER',
   });
 
+  await UserModel.create({ username: 'hello' });
+
   const doc = new UserModel({ username: 'user1' });
 
   console.log(doc);
+
+  // eslint-disable-next-line no-unused-expressions
+  doc._id;
 
   await mongoose.disconnect();
 }
