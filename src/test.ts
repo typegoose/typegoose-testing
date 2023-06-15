8d3dc6ec7f6189c1c3c028978392654ffe8e8191
hasezoey <hasezoey@gmail.com>
2023-06-15 19:49:11 +0200

    repro

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,35 @@
 // NodeJS: 19.9.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.2.0
 import * as mongoose from 'mongoose'; // mongoose@7.2.1
+import { ObjectId } from 'bson';
 
-class User {
-  @prop()
-  public username?: string;
+// 1. Create an interface representing a document in MongoDB.
+interface IUser {
+  name: string;
+  email: string;
+  // This needs to be Types.ObjectId so it will be different types
+  organizationId: mongoose.Types.ObjectId;
 }
 
-const UserModel = getModelForClass(User);
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new mongoose.Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  // And `Schema.Types.ObjectId` in the schema definition.
+  organizationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization' },
+});
+
+const User = mongoose.model<IUser>('User', userSchema);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
-
-  console.log(doc);
+  const user = new User();
+  // Property '_id' is missing in type ('bson").ObjectID' but required in type 'import("mongoose").Types.ObjectId'.
+  user.organizationId = new ObjectId();
 
   await mongoose.disconnect();
 }
