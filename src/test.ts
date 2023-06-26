4535d1a1b55d1cc7838b135bb271ddc6edc2f194
hasezoey <hasezoey@gmail.com>
2023-06-26 13:00:32 +0200

    update mongoose & typegoose

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,8 @@
 // NodeJS: 19.9.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.2.0
-import * as mongoose from 'mongoose'; // mongoose@7.2.1
+import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.3.0
+import * as mongoose from 'mongoose'; // mongoose@7.3.1
 
 class User {
   @prop()
