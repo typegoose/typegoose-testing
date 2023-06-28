02fbe0f5003ab89686cc479fb18b31531916193c
hasezoey <hasezoey@gmail.com>
2023-06-28 14:29:28 +0200

    example

--- src/test.ts
+++ src/test.ts
@@ -1,26 +1,36 @@
 // NodeJS: 20.2.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.3.0
+import { DocumentType, getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.3.0
 import * as mongoose from 'mongoose'; // mongoose@7.3.1
 
 class User {
   @prop()
   public username?: string;
+
+  public someInstanceMethod!: typeof someInstanceMethod;
 }
+
+User.prototype.someInstanceMethod = someInstanceMethod;
 
 const UserModel = getModelForClass(User);
 
 async function main() {
-  await mongoose.connect(`mongodb://localhost:27017/`, {
-    dbName: 'verifyMASTER',
-  });
+  // await mongoose.connect(`mongodb://localhost:27017/`, {
+  //   dbName: 'verifyMASTER',
+  // });
 
   const doc = new UserModel({ username: 'user1' });
 
+  console.log(doc.someInstanceMethod());
+
   console.log(doc);
 
-  await mongoose.disconnect();
+  // await mongoose.disconnect();
 }
 
 main();
+
+function someInstanceMethod(this: DocumentType<User>) {
+  return 'hello';
+}
