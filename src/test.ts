446f71603cc4918a2c37532eceb97f83a49cf256
hasezoey <hasezoey@gmail.com>
2023-07-04 12:14:52 +0200

    repro

--- src/test.ts
+++ src/test.ts
@@ -1,26 +1,31 @@
 // NodeJS: 20.2.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.3.0
 import * as mongoose from 'mongoose'; // mongoose@7.3.1
 
-class User {
-  @prop()
-  public username?: string;
+class SomeClass {
+  public test!: string;
+
+  public method() {}
+
+  // private testprop!: string;
+  private pmethod() {}
 }
 
-const UserModel = getModelForClass(User);
-
-async function main() {
-  await mongoose.connect(`mongodb://localhost:27017/`, {
-    dbName: 'verifyMASTER',
-  });
-
-  const doc = new UserModel({ username: 'user1' });
-
-  console.log(doc);
-
-  await mongoose.disconnect();
+interface Test {
+  class: SomeClass;
 }
 
-main();
+interface TestPOJO {
+  class: Pick<SomeClass, keyof SomeClass>;
+}
+
+// function is expected to return a "Test" interface with a instance of "SomeClass" at "class"
+function test(): /* Test */ TestPOJO {
+  // using undefined just as a placeholder for types
+  const t = undefined as any as mongoose.FlattenMaps<Test>;
+
+  t.class.method(); // type does not error, but is incorrect and does not work at runtime in .lean
+
+  return t; // return works without protected or private fields or methods
+}
