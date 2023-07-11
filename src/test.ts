e6d7714337bd3e274ad073f2fad5cfa424fe0c8a
hasezoey <hasezoey@gmail.com>
2023-07-11 21:10:20 +0200

    typegoose repro

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,164 @@
 // NodeJS: 20.2.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.3.0
+import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.3.0
 import * as mongoose from 'mongoose'; // mongoose@7.3.1
 
-class User {
-  @prop()
-  public username?: string;
+// const Types = ['typeWithPropA', 'typeWithPropB'];
+
+// const baseSchema = new mongoose.Schema({
+//   commonProp: {
+//     required: true,
+//     type: String,
+//   },
+//   type: {
+//     required: true,
+//     type: String,
+//     enum: Types,
+//   },
+// });
+
+// const propASchema = baseSchema.clone();
+// propASchema.add({
+//   propA: {
+//     required: true,
+//     type: String,
+//   },
+// });
+
+// const propBSchema = baseSchema.clone();
+// propBSchema.add({
+//   propB: {
+//     required: true,
+//     type: String,
+//   },
+// });
+
+// const containNestedSchema = new mongoose.Schema({
+//   containNestedCommonProp: {
+//     required: true,
+//     type: String,
+//   },
+//   listWithDiscriminators: {
+//     required: true,
+//     type: [baseSchema],
+//   },
+// });
+
+// {
+//   const path = containNestedSchema.path('listWithDiscriminators');
+//   (path as any).discriminator(Types[0], propASchema, Types[0]);
+//   (path as any).discriminator(Types[1], propBSchema, Types[1]);
+// }
+
+// const parentSchema = new mongoose.Schema({
+//   aRequiredProp: {
+//     required: true,
+//     type: String,
+//   },
+//   aList: {
+//     required: true,
+//     type: [containNestedSchema],
+//   },
+// });
+
+// const ParentModel = mongoose.model('parent', parentSchema);
+
+enum Types {
+  TypeWithPropA = 'typeWithPropA',
+  TypeWithPropB = 'typeWithPropB',
 }
 
-const UserModel = getModelForClass(User);
+@modelOptions({
+  schemaOptions: {
+    discriminatorKey: 'type',
+  },
+})
+abstract class Base {
+  @prop({ required: true })
+  commonProp!: string;
+
+  @prop({ enum: Types, required: true, type: String })
+  type!: Types;
+}
+
+class PropA extends Base {
+  @prop({ required: true })
+  propA!: string;
+}
+
+class PropB extends Base {
+  @prop({ required: true })
+  propB!: string;
+}
+
+class ContainNested {
+  @prop({ required: true })
+  containNestedCommonProp!: string;
+
+  @prop({
+    required: true,
+    type: Base,
+    discriminators: () => [
+      { type: PropA, value: Types.TypeWithPropA },
+      { type: PropB, value: Types.TypeWithPropB },
+    ],
+  })
+  listWithDiscriminators!: Base[];
+}
+
+class Parent {
+  @prop({ required: true })
+  aRequiredProp!: string;
+
+  @prop({ required: true, type: () => [ContainNested] })
+  aList!: ContainNested[];
+}
+
+const ParentModel = getModelForClass(Parent);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  mongoose.set('debug', true);
 
-  console.log(doc);
+  await // Inserting a document with an object of type "TypeWithPropA"
+  // in the list with discriminators
+  ParentModel.create({
+    aRequiredProp: 'hello',
+    aList: [
+      {
+        containNestedCommonProp: 'something-common',
+        listWithDiscriminators: [
+          {
+            type: Types.TypeWithPropA,
+            // type: Types[0],
+            commonProp: 'this is the common prop',
+            propA: 'this is propA',
+          },
+        ],
+      },
+    ],
+  });
+
+  await ParentModel.findOneAndUpdate(
+    {
+      aRequiredProp: 'hello',
+    },
+    {
+      'aList.$[].listWithDiscriminators.$[id].propA': 'hello 123123',
+    },
+    {
+      arrayFilters: [
+        {
+          'id.type': Types.TypeWithPropA,
+          // 'id.type': Types[0],
+        },
+      ],
+    }
+  ).exec();
 
   await mongoose.disconnect();
 }
