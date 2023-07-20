847a1110ac30f4b08046b96bb419a1fb6f01584a
hasezoey <hasezoey@gmail.com>
2023-07-20 21:29:10 +0200

    repro

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,79 @@
 // NodeJS: 20.2.0
 // MongoDB: 5.0 (Docker)
 // Typescript 4.9.5
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.4.0
+import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.4.0
 import * as mongoose from 'mongoose'; // mongoose@7.4.0
 
-class User {
-  @prop()
-  public username?: string;
+@modelOptions({
+  schemaOptions: {
+    _id: false,
+    discriminatorKey: 'component',
+  },
+})
+export class Component {
+  @prop({ required: true, type: String })
+  component!: string;
+
+  @prop({ required: true, type: String })
+  something!: string;
 }
 
-const UserModel = getModelForClass(User);
+export class ComponentA extends Component {
+  constructor() {
+    super();
+
+    this.component = 'a';
+  }
+
+  @prop({ trim: true, type: String })
+  name?: string;
+}
+
+class Module {
+  @prop({
+    required: true,
+    type: Component,
+    discriminators: () => [{ type: ComponentA, value: 'a' }],
+  })
+  components!: Component[];
+}
+
+export class Config {
+  @prop({ required: true, type: String })
+  type!: string;
+
+  @prop({ required: true, type: Module })
+  modules!: Module[];
+}
+
+const ConfigModel = getModelForClass(Config);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  mongoose.set('debug', true);
 
-  console.log(doc);
+  const doc = await ConfigModel.create({
+    type: 'test',
+    modules: [
+      { components: { component: 'a', something: 'test', name: 'testy' } },
+      { components: { component: 'a', something: 'test2', name: 'testy2' } },
+    ],
+  });
+
+  await ConfigModel.findOneAndUpdate(
+    {
+      _id: doc._id,
+    },
+    {
+      $set: { 'modules.$[].components.$[j].name': 'hello' },
+    },
+    {
+      arrayFilters: [{ 'j.something': 'test' }],
+    }
+  );
 
   await mongoose.disconnect();
 }
