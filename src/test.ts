b101403a07b3eba460ebb74a37f6977a1f169773
hasezoey <hasezoey@gmail.com>
2024-02-07 18:31:46 +0100

    update typescript version

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 // NodeJS: 20.8.0
 // MongoDB: 5.0 (Docker)
-// Typescript 4.9.5
+// Typescript 5.3.3
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@11.6.0
 import * as mongoose from 'mongoose'; // mongoose@7.6.4
 
