a012b6a520da2dd6dfc1fdcc762bb2b306429030
hasezoey <hasezoey@gmail.com>
2024-05-03 12:44:31 +0200

    cannot reproduce

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,27 @@
 // NodeJS: 21.6.2
 // MongoDB: 5.0 (Docker)
 // Typescript 5.3.3
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.4.0
+import { ReturnModelType, getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.4.0
+import { BeAnObject } from '@typegoose/typegoose/lib/types';
 import * as mongoose from 'mongoose'; // mongoose@8.3.3
 
-class User {
+class AdminLogEntity {
   @prop()
   public username?: string;
 }
-
-const UserModel = getModelForClass(User);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const session: mongoose.mongo.ClientSession = await mongoose.startSession();
+  const adminLogModel: ReturnModelType<typeof AdminLogEntity, BeAnObject> = getModelForClass(AdminLogEntity);
+  const options: mongoose.CreateOptions = { session };
 
-  console.log(doc);
+  const adminLogEntity = await adminLogModel.create({}, options);
+
+  console.log('entity', adminLogEntity);
 
   await mongoose.disconnect();
 }
