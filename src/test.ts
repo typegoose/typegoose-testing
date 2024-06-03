8cce35fe2d6fa920270e4e4c5a2e647f035de8ba
hasezoey <hasezoey@gmail.com>
2024-06-03 12:32:57 +0200

    cannot reproduce

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 // NodeJS: 21.6.2
 // MongoDB: 5.0 (Docker)
 // Typescript 5.3.3
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.4.0
+import { DocumentType, getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.4.0
 import * as mongoose from 'mongoose'; // mongoose@8.3.3
 
 class User {
@@ -11,14 +11,20 @@ class User {
 
 const UserModel = getModelForClass(User);
 
+function processUser(user: DocumentType<User>): DocumentType<User> {
+  // avoid parameter unused warning & function must return something warning
+  return user;
+}
+
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const user = await UserModel.findOne({}).orFail().exec();
+  const processedUser = processUser(user); // no errors
 
-  console.log(doc);
+  console.log(processedUser);
 
   await mongoose.disconnect();
 }
