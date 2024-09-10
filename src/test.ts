487e7e47d73e5d5f61c9f5ed17374d3d96f0fee1
hasezoey <hasezoey@gmail.com>
2024-09-10 11:35:54 +0200

    cannot reproduce

--- src/test.ts
+++ src/test.ts
@@ -4,21 +4,38 @@
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.7.0
 import * as mongoose from 'mongoose'; // mongoose@8.6.1
 
-class User {
+// note that this class has to be defined before being used in Animal
+// alternatively you could also use "@prop({ type: () => Properties })" instead if defining the "Properties" class after "Animal"
+class Properties {
   @prop()
-  public username?: string;
+  public hasEaten?: boolean;
 }
 
-const UserModel = getModelForClass(User);
+class Animal {
+  @prop()
+  public name?: string;
+
+  @prop()
+  public properties?: Properties;
+}
+
+const AnimalModel = getModelForClass(Animal);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  // directly defining everything
+  const doc1 = await AnimalModel.create({ name: 'Kitty', properties: { hasEaten: true } });
 
-  console.log(doc);
+  console.log(doc1);
+
+  // properties via some kind of argument
+  const props = { hasEaten: false };
+  const doc2 = await AnimalModel.create({ name: 'Kitty', properties: props });
+
+  console.log(doc2);
 
   await mongoose.disconnect();
 }
