05d43c7561ce84b5681b679e68d3cce3ce154468
hasezoey <hasezoey@gmail.com>
2024-11-17 13:49:33 +0100

    repro fix

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,46 @@
 // NodeJS: 22.8.0
 // MongoDB: 7.0 (Docker)
 // Typescript 5.3.3
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.9.0
+import { defaultClasses, getDiscriminatorModelForClass, getModelForClass, index, modelOptions, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.9.0
 import * as mongoose from 'mongoose'; // mongoose@8.8.0
 
-class User {
-  @prop()
-  public username?: string;
+@index({ make: 1, color: 1 }, { unique: true, background: false })
+@modelOptions({
+  schemaOptions: { collection: 'cars', discriminatorKey: 'make' },
+})
+class Car extends defaultClasses.TimeStamps {
+  @prop({ required: true })
+  public make!: string;
+
+  @prop({ required: true })
+  public color!: string;
 }
 
-const UserModel = getModelForClass(User);
+const CarModel = getModelForClass(Car);
+
+@modelOptions({ options: { disableLowerIndexes: true } })
+class Mazda extends Car {}
+
+const MazdaModel = getDiscriminatorModelForClass(CarModel, Mazda, 'mazda');
+
+@modelOptions({ options: { disableLowerIndexes: true } })
+class Cadillac extends Car {}
+
+const CadillacModel = getDiscriminatorModelForClass(CarModel, Cadillac, 'cadi');
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  await CarModel.syncIndexes();
 
-  console.log(doc);
+  // the following 2 dont create any indexes as they dont have any defined
+  await MazdaModel.syncIndexes();
+  await CadillacModel.syncIndexes();
+
+  await MazdaModel.create({ color: 'blue1' });
+  await CadillacModel.create({ color: 'red1' });
 
   await mongoose.disconnect();
 }
