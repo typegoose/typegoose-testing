25f5c4127e2c9ef3b27b0e6e3dbfa4aa4bcff143
hasezoey <hasezoey@gmail.com>
2024-12-12 16:15:41 +0100

    showcase 1

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,85 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // NodeJS: 22.8.0
 // MongoDB: 7.0 (Docker)
 // Typescript 5.3.3
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.9.0
+import { DocumentType } from '@typegoose/typegoose';
+import { getModelForClass, prop, Ref } from '@typegoose/typegoose'; // @typegoose/typegoose@12.9.0
 import * as mongoose from 'mongoose'; // mongoose@8.8.0
 
-class User {
+class TokenPojo {
   @prop()
-  public username?: string;
+  public token?: string;
+
+  @prop()
+  public expiresAt?: string;
+
+  constructor(anatomy: Partial<TokenPojo> = {}) {}
+
+  hash?(this: DocumentType<TokenPojo>) {
+    return this._id === undefined ? '' : this._id;
+  }
 }
 
-const UserModel = getModelForClass(User);
+const TokenPojoModel = getModelForClass(TokenPojo);
+
+class EncipheredTokensPojo {
+  @prop({ ref: () => TokenPojo })
+  public accessToken?: Ref<TokenPojo>;
+
+  @prop({ ref: () => TokenPojo })
+  public refreshToken?: Ref<TokenPojo>;
+
+  constructor(anatomy: Partial<EncipheredTokensPojo> = {}) {}
+
+  hash?(this: DocumentType<TokenPojo>) {
+    return this._id === undefined ? '' : this._id;
+  }
+}
+
+export const EncipheredTokensPojoModel = getModelForClass(EncipheredTokensPojo);
+
+class UserPojo {
+  @prop({ ref: () => EncipheredTokensPojo })
+  public tokens?: Ref<EncipheredTokensPojo>;
+
+  constructor(anatomy: Partial<UserPojo> = {}) {}
+
+  hash?(this: DocumentType<TokenPojo>): mongoose.Types.ObjectId | string {
+    return this._id === undefined ? '' : this._id;
+  }
+}
+
+const UserPojoModel = getModelForClass(UserPojo);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const expiresAtInt = Date.now() + 300000; // 5 minutes later
+  const expiresAt = new Date(expiresAtInt).toISOString();
 
-  console.log(doc);
+  const accessToken = new TokenPojoModel({
+    token: 'dummy token',
+    expiresAt,
+  });
+
+  const refreshToken = new TokenPojoModel({
+    token: 'dummy token',
+    expiresAt,
+  });
+
+  await Promise.all([accessToken.save(), refreshToken.save()]);
+
+  const user = new UserPojoModel({
+    tokens: new EncipheredTokensPojoModel({
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    }),
+  });
+
+  await (user.tokens as DocumentType<EncipheredTokensPojo>).save();
+  await user.save();
 
   await mongoose.disconnect();
 }
