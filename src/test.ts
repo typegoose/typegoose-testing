a6cc21fa0677748f3aeb925e79782879eb5704f1
hasezoey <hasezoey@gmail.com>
2024-12-17 16:22:11 +0100

    cannot reproduce typescript 5.3

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,38 @@
 // NodeJS: 22.8.0
 // MongoDB: 7.0 (Docker)
 // Typescript 5.3.3
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.10.1
+import { ReturnModelType, getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@12.10.1
 import * as mongoose from 'mongoose'; // mongoose@8.9.1
 
-class User {
+class Character {
   @prop()
-  public username?: string;
+  public name?: string;
+
+  @prop()
+  public realmId?: string;
+
+  public static async findByRealmAndName(this: ReturnModelType<typeof Character>, realmId: string, name: string) {
+    return this.findOne({
+      name,
+      realm: realmId,
+    })
+      .lean()
+      .exec();
+  }
 }
 
-const UserModel = getModelForClass(User);
+const CharacterModel = getModelForClass(Character);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const doc = new CharacterModel({ username: 'user1' });
 
   console.log(doc);
+
+  await CharacterModel.findByRealmAndName('test', 'test');
 
   await mongoose.disconnect();
 }
