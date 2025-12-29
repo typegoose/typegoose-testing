5b57a729c6a40bffe669c787501e836d720e6e84
hasezoey <hasezoey@gmail.com>
2025-12-29 14:12:04 +0100

    repro mongoose

--- src/test.ts
+++ src/test.ts
@@ -1,65 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // NodeJS: 25.2.1
 // MongoDB: 7.0 (Docker)
 // Typescript 5.9.3
-import { getModelForClass, prop, Ref } from '@typegoose/typegoose'; // @typegoose/typegoose@13.0.0
+// import { getModelForClass, prop, Ref } from '@typegoose/typegoose'; // @typegoose/typegoose@13.0.0
 import * as mongoose from 'mongoose'; // mongoose@9.0.2
 
-class User {
-  @prop({ required: true })
-  public name!: string;
-
-  @prop({ required: true, unique: true })
-  public email!: string;
+interface Schema2 {
+  test: string;
+}
+interface Schema1 {
+  test: string;
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  refTo: mongoose.PopulatedDoc<mongoose.Document<mongoose.Types.ObjectId, {}, Schema2> & Schema2, mongoose.Types.ObjectId>;
 }
 
-const UserModel = getModelForClass(User);
-
-class Campaign {
-  @prop({ required: true })
-  public name!: string;
-
-  @prop({ required: true, ref: () => User })
-  public user!: Ref<User>;
-}
-
-const CampaignModel = getModelForClass(Campaign);
+const schema1 = new mongoose.Schema({
+  test: String,
+  refTo: {
+    ref: 'Model2',
+    type: mongoose.Types.ObjectId,
+  },
+});
+const schema2 = new mongoose.Schema({
+  test: String,
+});
+const model1 = mongoose.model<Schema1>('Model1', schema1);
+const model2 = mongoose.model<Schema2>('Model2', schema2);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  // Create a test user
-  const user = await UserModel.create({
-    name: 'Test User',
-    email: 'test@example.com',
-  });
-  console.log('Created user:', user._id);
+  const doc2 = await model2.create({ test: 'hello' });
+  const doc1 = await model1.create({ test: 'ish', refTo: doc2._id });
 
-  // Create a test campaign
-  const campaign = await CampaignModel.create({
-    name: 'Test Campaign',
-    user: user._id,
-  });
-  console.log('Created campaign:', campaign._id);
-
-  // BUG: Filtering with string causes TypeScript error
-  const userIdAsString = user._id.toString();
-
-  // TypeScript error: Type 'string' is not assignable to type 'Ref<User>'
-  const campaignsByString = await CampaignModel.find({ user: userIdAsString });
-  console.log('Campaigns found with string:', campaignsByString.length);
-
-  // BUG: Filtering with ObjectId causes TypeScript error
-  const userIdAsObjectId = new mongoose.Types.ObjectId(userIdAsString);
-
-  // TypeScript error: Type 'ObjectId' is not assignable to type 'Ref<User>'
-  const campaignsByObjectId = await CampaignModel.find({ user: userIdAsObjectId });
-  console.log('Campaigns found with ObjectId:', campaignsByObjectId.length);
-
-  // The same happens here
-  const campaignsByRef = await CampaignModel.find({ user: user._id });
-  console.log('Campaigns found with Ref:', campaignsByRef.length);
+  await model1.find({ refTo: doc2 });
+  await model1.find({ refTo: doc2._id });
+  await model1.find({ refTo: doc2._id.toString() });
 
   await mongoose.disconnect();
 }
