521c604be9c38ca0713f4684c1522c13ccc172e3
hasezoey <hasezoey@gmail.com>
2026-01-16 15:00:57 +0100

    not verified

--- src/test.ts
+++ src/test.ts
@@ -4,21 +4,49 @@
 import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@13.0.0
 import * as mongoose from 'mongoose'; // mongoose@9.0.2
 
-class User {
+class TestArray {
   @prop()
-  public username?: string;
+  public id!: number;
+
+  @prop()
+  public v!: number;
 }
 
-const UserModel = getModelForClass(User);
+class TestData {
+  @prop()
+  public _id!: string;
+
+  @prop({ type: () => [TestArray], _id: false })
+  public array: TestArray[];
+}
+
+const TestModel = getModelForClass(TestData, {
+  schemaOptions: { collection: 'test' },
+});
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  await TestModel.deleteOne({ _id: 'test' });
+  await TestModel.create({ _id: 'test', array: [] });
 
-  console.log(doc);
+  const doc = await TestModel.findById('test').orFail();
+
+  // [ { "id" : 1, "v" : 0 }, { "id" : 2, "v" : 0 } ]
+  doc.array = [
+    { id: 1, v: 0 },
+    { id: 2, v: 0 },
+  ];
+  await doc.save(); // [ { "id" : 2, "v" : 0 }, { "id" : 1, "v" : 0 } ]
+  const fetched1 = await TestModel.findById(doc).orFail().lean();
+  console.log('first', JSON.stringify(doc) === JSON.stringify(fetched1));
+
+  doc.array[0].v = 999;
+  await doc.save(); // nothing changed [ { "id" : 2, "v" : 0 }, { "id" : 1, "v" : 0 } ]
+  const fetched2 = await TestModel.findById(doc).orFail().lean();
+  console.log('second', JSON.stringify(doc) === JSON.stringify(fetched2));
 
   await mongoose.disconnect();
 }
