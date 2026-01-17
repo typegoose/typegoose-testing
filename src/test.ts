8314dd199bfc3eeb092438367708bd877701919b
hasezoey <hasezoey@gmail.com>
2026-01-17 12:07:34 +0100

    working

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,30 @@
 // NodeJS: 25.2.1
 // MongoDB: 7.0 (Docker)
 // Typescript 5.9.3
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@13.0.0
-import * as mongoose from 'mongoose'; // mongoose@9.0.2
+import { DocumentType, getModelForClass, plugin, prop, ReturnModelType } from '@typegoose/typegoose'; // @typegoose/typegoose@13.0.0
+import mongoose from 'mongoose'; // mongoose@9.0.2
+import paginatePlugin from 'mongoose-paginate-v2';
 
+@plugin(paginatePlugin)
 class User {
   @prop()
   public username?: string;
 }
 
-const UserModel = getModelForClass(User);
+const UserModel = getModelForClass(User) as ReturnModelType<typeof User> & Pick<mongoose.PaginateModel<DocumentType<User>>, 'paginate'>;
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const result = await UserModel.paginate({}, { page: 0, limit: 10 });
+  console.log('docs', result.docs);
 
-  console.log(doc);
+  if (result.docs.length > 0) {
+    const id = result.docs[0]._id;
+    console.log('id of first', id);
+  }
 
   await mongoose.disconnect();
 }
