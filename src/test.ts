806c2e723831711ccf3c4a577d5fc34080ab7f0f
hasezoey <hasezoey@gmail.com>
2026-01-19 12:20:14 +0100

    repro

--- src/test.ts
+++ src/test.ts
@@ -30,23 +30,37 @@ async function main() {
   });
 
   await TestModel.deleteOne({ _id: 'test' });
-  await TestModel.create({ _id: 'test', array: [] });
+  await TestModel.create({
+    _id: 'test',
+    array: [
+      { id: 1, v: 0 },
+      { id: 2, v: 0 },
+    ],
+  });
 
   const doc = await TestModel.findById('test').orFail();
 
-  // [ { "id" : 1, "v" : 0 }, { "id" : 2, "v" : 0 } ]
-  doc.array = [
-    { id: 1, v: 0 },
-    { id: 2, v: 0 },
-  ];
+  // switch positions
+  doc.array = [doc.array[1], doc.array[0]];
   await doc.save(); // [ { "id" : 2, "v" : 0 }, { "id" : 1, "v" : 0 } ]
   const fetched1 = await TestModel.findById(doc).orFail().lean();
-  console.log('first', JSON.stringify(doc) === JSON.stringify(fetched1), doc);
+  // first true { _id: 'test', array: [ { id: 2, v: 0 }, { id: 1, v: 0 } ], __v: 1 }
+  console.log('first', JSON.stringify(doc) === JSON.stringify(fetched1));
+  console.log('doc', doc);
+  console.log('fetched1', fetched1);
 
   doc.array[0].v = 999;
+  // modified [ 'array', 'array.1', 'array.1.v' ]
+  // ^ should be "array.0"
+  console.log('modified', doc.modifiedPaths());
   await doc.save(); // nothing changed [ { "id" : 2, "v" : 0 }, { "id" : 1, "v" : 0 } ]
   const fetched2 = await TestModel.findById(doc).orFail().lean();
-  console.log('second', JSON.stringify(doc) === JSON.stringify(fetched2), doc);
+  // second false { _id: 'test', array: [ { id: 2, v: 999 }, { id: 1, v: 0 } ], __v: 1 }
+  console.log('second', JSON.stringify(doc) === JSON.stringify(fetched2));
+  // { _id: 'test', array: [ { id: 2, v: 999 }, { id: 1, v: 0 } ], __v: 1 }
+  console.log('doc', doc);
+  // { _id: 'test', array: [ { id: 2, v: 0 }, { id: 1, v: 0 } ], __v: 1 }
+  console.log('fetched2', fetched2);
 
   await mongoose.disconnect();
 }
