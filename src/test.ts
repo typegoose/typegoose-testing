f476231b8eb3481ab31e3473bd3ae0a90792acd2
hasezoey <hasezoey@gmail.com>
2026-03-03 12:20:23 +0100

    repro

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,27 @@
 // NodeJS: 25.2.1
 // MongoDB: 7.0 (Docker)
 // Typescript 5.9.3
-import { getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@13.2.0
+import { DocumentType, getModelForClass, prop } from '@typegoose/typegoose'; // @typegoose/typegoose@13.2.0
 import * as mongoose from 'mongoose'; // mongoose@9.2.3
 
-class User {
+class Foo {
   @prop()
-  public username?: string;
+  name: string;
 }
 
-const UserModel = getModelForClass(User);
+const FooModel = getModelForClass(Foo);
 
 async function main() {
   await mongoose.connect(`mongodb://localhost:27017/`, {
     dbName: 'verifyMASTER',
   });
 
-  const doc = new UserModel({ username: 'user1' });
+  const foo: DocumentType<Foo> = new FooModel({
+    name: 'test',
+  });
 
-  console.log(doc);
+  // Typescript error when using DocumentType<T>
+  await FooModel.bulkSave([foo]);
 
   await mongoose.disconnect();
 }
